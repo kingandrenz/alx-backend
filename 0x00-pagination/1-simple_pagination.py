06e9e6005fc746100f59859e198981fe36905e22
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-24 17:41:50 +0000

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" task 1 """
+
+
+from typing import List
+import math
+import csv
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ get pages index """
+
+        assert isinstance(page,
+                          int) and page > 0, "page must be a positive integer."
+        assert isinstance(page_size,
+                          int) and page_size > 0, "pagesize mustbe positive"
+
+        data = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
