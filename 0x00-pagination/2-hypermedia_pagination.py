ad1649654a21639ef65bd3d6c6a6e08c62a7061e
Anthony Kanu <kanuchibueze@gmail.com>
2023-07-24 19:04:24 +0000

    task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" task 1 """
+
+
+from typing import List, Dict, Union
+import math
+import csv
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ get pages index """
+
+        assert isinstance(page,
+                          int) and page > 0, "page must be a positive integer."
+        assert isinstance(page_size,
+                          int) and page_size > 0, "pagesize mustbe positive"
+
+        data = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size:
+                  int = 10) -> Dict[str, Union[int, List[List], None]]:
+        """ returns a dictionary """
+
+        assert isinstance(page, int) and page > 0, "page must be positive"
+        assert isinstance(page_size,
+                          int) and page_size > 0, "pageSize must be positive"
+
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
